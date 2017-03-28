63ba82519b4eea52f30f3f99fbd0307648737221
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-03-29 00:49:26 +0200

    add explanation to withData HOC slide

--- pages/17.js
+++ pages/17.js
@@ -8,6 +8,9 @@ export default ({url}) => (
   <Slide url={url}>
     <Header apollo />
     <Layout>
+      {md({code: InlineCode})`
+\`withData\` HOC (simplified code, [full version](https://github.com/zeit/next.js/blob/master/examples/with-apollo/lib/withData.js)):
+      `}
       <Code>{include('../snippets/withData.js')}</Code>
       {md({code: InlineCode})`
 \`getInitialProps\` performs a \`getDataFromTree\` when server rendering.
